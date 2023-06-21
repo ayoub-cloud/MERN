1f95b969d6fed5715b6786ed29b67149d7d99402
ayoub-cloud <ayoubb06@hotmail.com>
2023-06-21 15:07:53 +0200

    first react-app

---
+++ week01/Day03/Core/react/first-react/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+ import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello Dojo</h1>
+      <h2>Todo List:</h2>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
