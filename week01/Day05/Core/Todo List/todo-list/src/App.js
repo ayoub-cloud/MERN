8789fdd08e2118ff7e98b6e628ce2c8479c37fa2
ayoub-cloud <ayoubb06@hotmail.com>
2023-06-25 01:27:44 +0200

    todo-list

---
+++ week01/Day05/Core/Todo List/todo-list/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import Task from './components/Task';
+import './App.css';
+
+
+function App() {
+
+  const [ input, setInput ] = useState("");
+  const [ tasks, setTasks ] = useState(JSON.parse(localStorage.getItem('tasks')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+  })
+
+  const handleAddTask = e => {
+    e.preventDefault();
+    let t = {
+      title : input,
+      isCompleted : false
+    }
+    setTasks([...tasks,t])
+    setInput("")
+  }
+
+  const handleToggleCheck = (e, idx) => {
+    let t = tasks[idx];
+    t.isCompleted = !t.isCompleted;
+    setTasks([...tasks.slice(0,idx),t,...tasks.slice(idx+1)]);
+  }
+
+  const handleDestroyTask = (e, idx) => {
+    setTasks([...tasks.slice(0, idx),...tasks.slice(idx + 1)])
+  }  
+
+  return (
+    <div className="App">
+      <form onSubmit={ handleAddTask } className="mx-auto my-5 col-4">
+        <h2 className="header text-center">To Do List:</h2>
+        <div className="form-group d-flex">
+          <input 
+            type="text" 
+            className="form-control"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <input type="submit" className="btn btn-secondary btn-outline-dark" value="Add"/>
+        </div>
+
+      </form>
+      <div className="">
+        {
+          tasks.map(( t, i ) => {
+              return <Task
+                        idx={i}
+                        handleToggleCheck={handleToggleCheck}
+                        task={t}
+                        key={i}
+                        handleDestroyTask={handleDestroyTask}
+                    />
+          })
+        }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ week01/Day05/Core/Todo List/todo-list/src/components/Task.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Task =( props) => {
+
+    const { task, handleToggleCheck, handleDestroyTask, idx } = props;
+
+    return (
+        <div className="display">
+            <div className="col-4 mx-auto d-flex justify-content-between">
+                 <br/>
+                 <input 
+                    type="checkbox" 
+                    className="form-control col-1" 
+                    checked={ task.isCompleted }
+                    onChange={(e) => handleToggleCheck( e, idx )}
+                />
+                <h2 className="col-2" style={ task.isCompleted ? { textDecoration:"line-through"} : {} }>{ task.title }</h2>
+
+                <button className="btn btn-dark text-light btn-small" onClick={(e) => handleDestroyTask(e, idx)}>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Task
